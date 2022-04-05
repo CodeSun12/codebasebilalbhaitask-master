87b700ca7fa2a3fdcf79b8804a403d71cb8f3b21
codesun <codebasesun@gmail.com>
2022-04-05 14:38:19 +0500

    Fetch Location

--- lib/LiveTracking/get_live_location_tracking_values.dart
+++ lib/LiveTracking/get_live_location_tracking_values.dart
@@ -1,14 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:codebasebilalbhaitask/model/GetMyModel.dart';
-import 'package:codebasebilalbhaitask/screen/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'package:location/location.dart';
-import 'package:provider/provider.dart';
-
-import '../practice/getdata.dart';
 
 
 class GetLiveLocationTracking extends StatefulWidget {
@@ -21,40 +17,75 @@ class GetLiveLocationTracking extends StatefulWidget {
 class _GetLiveLocationTrackingState extends State<GetLiveLocationTracking> {
 
 
-  List _loadedLatAndLong = [];
-
-
   // Get Location From Server Method
-  List <GetMyModel> postList = [];
+  List  <GetMyModel> postList = [];
+  List<LatLng> list = [];
+  double latitude = 0;
+  double longitude = 0;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  Completer<GoogleMapController> _controller = Completer();
+
+
+  List _data = [];
+
 
   getPostApi() async {
     final response = await http.get(
         Uri.parse('http://codebase.pk:8800/api/location/'));
-    var data = jsonDecode(response.body);
+    var data = jsonDecode(response.body.toString());
+    print(data);
+    setState(() {
+      if(data != null){
+        _data = data;
+      }
+    });
     if (response.statusCode == 200) {
       for (var i in data) {
         postList.add(GetMyModel.fromJson(i));
+        setState(() {
+          for(int x = 0; x < _data.length; x++){
+            var latitude = _data[x]['latitude'];
+            var longitude = _data[x]['longitude'];
+            String timestamp  = _data[x]['timestamp'];
+            LatLng myLocation = LatLng(latitude!, longitude!);
+            if (list.contains(myLocation)){
+              list.clear();
+              list.add(myLocation);
+            }else{
+              list.add(myLocation);
+            }
+            addMarker(list[x], x);
+          }
+        });
       }
-      return postList;
-    } else {
-      return postList;
     }
   }
 
+//Adding Index here as an argument
+  void addMarker(loc, index) {
+    //Making this markerId dynamic
+    final MarkerId markerId = MarkerId('Marker  $index');
 
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(loc.latitude, loc.longitude),
+      infoWindow: InfoWindow(title: 'MyLocation'),
+    );
 
-
-  Future<void> fetchData() async{
-    final response = await http.get(Uri.parse('http://codebase.pk:8800/api/location/'));
-    final data = jsonDecode(response.body);
     setState(() {
-      _loadedLatAndLong = data;
+      // adding a new marker to map
+      markers[markerId] = marker;
+      //print(marker);
     });
-    print(_loadedLatAndLong[10]);
   }
 
 
-  static final CameraPosition intialCameraPosition = CameraPosition(target: LatLng(30.1916903, 71.4430995), zoom: 14);
+
+
+
+
+
+  static final CameraPosition initialCameraPosition = CameraPosition(target: LatLng(30.1916903, 71.4430995), zoom: 14);
   GoogleMapController? googleMapController;
 
 
@@ -63,64 +94,32 @@ class _GetLiveLocationTrackingState extends State<GetLiveLocationTracking> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepOrangeAccent,
-        centerTitle: true,
-        title: Text("Rest Location From Server"),
+        title: Text("Get Location From Server"),
       ),
-      body:
-      FutureBuilder(
-        future: fetchData(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return Container(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          } else {
-            return ListView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, i) {
-                lat = snapshot.data[10].latitude;
-                long = snapshot.data[10].longitude;
-                return Container(
-                  height: MediaQuery.of(context).size.height / 2.0,
-                  child: GoogleMap(
-                    initialCameraPosition: intialCameraPosition,
-                    mapType: MapType.normal,
-                    myLocationButtonEnabled: false,
-                    myLocationEnabled: true,
-                    zoomControlsEnabled: true,
-                    zoomGesturesEnabled: true,
-                    onMapCreated: (GoogleMapController controller){
-                      controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat!, long!))));
-                      googleMapController = controller;
-                    },
-                  ),
-                );
-              },
-            );
-          }
+      body: GoogleMap(
+        mapType: MapType.normal,
+        markers: Set.of(markers.values),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomGesturesEnabled: true,
+        initialCameraPosition: initialCameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          googleMapController = controller;
         },
       ),
-
-
-
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          getPostApi();
+        },
+        label: const Text('Refresh'),
+        icon: const Icon(Icons.refresh),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
-
-// ListTile(
-// title: Text("Latitude  $lat ", style: TextStyle(
-// color: Colors.black87, fontWeight: FontWeight.bold),),
-// subtitle: Text("Longitude  $long ", style: TextStyle(
-// color: Colors.black87, fontWeight: FontWeight.bold),),
-// trailing: Text("TimeStamp  ${snapshot.data[i].timestamp} ",
-// style: TextStyle(
-// color: Colors.black87, fontWeight: FontWeight.bold),),
-//
-// );
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,173 +1,23 @@
-// import 'package:codebasebilalbhaitask/practice/getdata.dart';
-// import 'package:codebasebilalbhaitask/practice/just_for_check.dart';
-// import 'package:codebasebilalbhaitask/practice/markers_location.dart';
-// import 'package:codebasebilalbhaitask/practice/shakeeb_get_location.dart';
-// import 'package:codebasebilalbhaitask/screen/my_current_location.dart';
-// import 'package:codebasebilalbhaitask/screen/provider.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-//
-// import 'LiveTracking/get_live_location_tracking_values.dart';
-// import 'LiveTracking/live_location_tracking.dart';
-// import 'get_loc.dart';
-//
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider<CheckoutProvider>(create: (context) => CheckoutProvider()),
-//       ],
-//       child: const MaterialApp(
-//         title: "Current Location",
-//         debugShowCheckedModeBanner: false,
-//         home: JustForCheck(),
-//       ),
-//     );
-//   }
-// }
-//
-
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_place_picker/google_maps_place_picker.dart';
-// Your api key storage.
-import 'keys.dart';
+import 'LiveTracking/get_live_location_tracking_values.dart';
+import 'LiveTracking/just_for_check.dart';
 
-void main() => runApp(MyApp());
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // Light Theme
-  final ThemeData lightTheme = ThemeData.light().copyWith(
-    // Background color of the FloatingCard
-    cardColor: Colors.white,
-    buttonTheme: ButtonThemeData(
-      // Select here's button color
-      buttonColor: Colors.black,
-      textTheme: ButtonTextTheme.primary,
-    ),
-  );
-
-  // Dark Theme
-  final ThemeData darkTheme = ThemeData.dark().copyWith(
-    // Background color of the FloatingCard
-    cardColor: Colors.grey,
-    buttonTheme: ButtonThemeData(
-      // Select here's button color
-      buttonColor: Colors.yellow,
-      textTheme: ButtonTextTheme.primary,
-    ),
-  );
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Google Map Place Picker Demo',
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.light,
-      home: HomePage(),
+      title: "Current Location",
       debugShowCheckedModeBanner: false,
+      home: JustForCheck(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key key}) : super(key: key);
-
-  static final kInitialPosition = LatLng(-33.8567844, 151.213108);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  PickResult selectedPlace;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Google Map Place Picer Demo"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                child: Text("Load Google Map"),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return PlacePicker(
-                          apiKey: APIKeys.apiKey,
-                          initialPosition: HomePage.kInitialPosition,
-                          useCurrentLocation: true,
-                          selectInitialPosition: true,
-
-                          //usePlaceDetailSearch: true,
-                          onPlacePicked: (result) {
-                            selectedPlace = result;
-                            Navigator.of(context).pop();
-                            setState(() {});
-                          },
-                          //forceSearchOnZoomChanged: true,
-                          //automaticallyImplyAppBarLeading: false,
-                          //autocompleteLanguage: "ko",
-                          //region: 'au',
-                          //selectInitialPosition: true,
-                          // selectedPlaceWidgetBuilder: (_, selectedPlace, state, isSearchBarFocused) {
-                          //   print("state: $state, isSearchBarFocused: $isSearchBarFocused");
-                          //   return isSearchBarFocused
-                          //       ? Container()
-                          //       : FloatingCard(
-                          //           bottomPosition: 0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
-                          //           leftPosition: 0.0,
-                          //           rightPosition: 0.0,
-                          //           width: 500,
-                          //           borderRadius: BorderRadius.circular(12.0),
-                          //           child: state == SearchingState.Searching
-                          //               ? Center(child: CircularProgressIndicator())
-                          //               : RaisedButton(
-                          //                   child: Text("Pick Here"),
-                          //                   onPressed: () {
-                          //                     // IMPORTANT: You MUST manage selectedPlace data yourself as using this build will not invoke onPlacePicker as
-                          //                     //            this will override default 'Select here' Button.
-                          //                     print("do something with [selectedPlace] data");
-                          //                     Navigator.of(context).pop();
-                          //                   },
-                          //                 ),
-                          //         );
-                          // },
-                          // pinBuilder: (context, state) {
-                          //   if (state == PinState.Idle) {
-                          //     return Icon(Icons.favorite_border);
-                          //   } else {
-                          //     return Icon(Icons.favorite);
-                          //   }
-                          // },
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              selectedPlace == null ? Container() : Text(selectedPlace.formattedAddress ?? ""),
-            ],
-          ),
-        ));
-  }
-}
